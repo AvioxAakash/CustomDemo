ae5d2e3938e9924eaa246cd78829065e620f5c82
root <aakashaviox@gmail.com>
2019-04-01 11:53:15 +0530

    Activate Library

---
+++ app/src/main/java/com/avioxofficial/customdemo/MyMath.java
@@ -0,0 +1,15 @@
+package com.avioxofficial.customdemo;
+
+public class MyMath
+{
+
+    public static int Plus(int a,int b)
+    {
+        return a+b;
+    }
+
+    public static int Minus(int a,int b)
+    {
+        return a-b;
+    }
+}
